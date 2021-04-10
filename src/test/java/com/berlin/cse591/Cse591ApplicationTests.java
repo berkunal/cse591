88b2778b23c5652d77ea9e083aff0056fac54486
Berk Unal <cesmeli.berk@gmail.com>
2021-04-10 18:04:00 +0300

    Initial commit

---
+++ src/test/java/com/berlin/cse591/Cse591ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.berlin.cse591;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cse591ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
